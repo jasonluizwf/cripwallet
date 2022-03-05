0bec4e40ae758027156d69830c8e5864c6fdc7fb
Victor <victorluizfacul@gmail.com>
2022-03-05 14:12:45 -0300

    Commit teste

--- src/main/java/com/projetointegrador/cripwallet/CripwalletApplication.java
+++ src/main/java/com/projetointegrador/cripwallet/CripwalletApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CripwalletApplication {
 
 	public static void main(String[] args) {
+		//teste
 		SpringApplication.run(CripwalletApplication.class, args);
 	}
 
