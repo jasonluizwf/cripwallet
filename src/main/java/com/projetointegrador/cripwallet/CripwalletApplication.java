004a1493c395042f148dd991fca0c06fec9e1e76
Victor <victorluizfacul@gmail.com>
2022-03-05 13:49:19 -0300

    Criando projeto

---
+++ src/main/java/com/projetointegrador/cripwallet/CripwalletApplication.java
@@ -0,0 +1,13 @@
+package com.projetointegrador.cripwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CripwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CripwalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetointegrador/cripwallet/CripwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetointegrador.cripwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CripwalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
